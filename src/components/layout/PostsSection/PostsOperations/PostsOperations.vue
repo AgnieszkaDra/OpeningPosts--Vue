3ada22338d2aa2b96112a9ae929829d2584d2201
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-03-13 10:23:32 +0100

    add line comments for code to search posts

--- src/components/layout/PostsSection/PostsOperations/PostsOperations.vue
+++ src/components/layout/PostsSection/PostsOperations/PostsOperations.vue
@@ -3,9 +3,8 @@
     class="operations"
   >
     <h4 class="operations__headline">Znajdź post:</h4>
-
     <FilterInput class="operations__filter"/>
-    <SearchInput class="operations__search"/>
+    <SearchInput class="operations__search" v-model="search"/>
   </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -8,6 +8,7 @@ export const usePostStore = defineStore({
     postsData: [],
     postsPerPage: 3,
     currentSelect: [],
+    currentSearch: ''
   }),
   actions: {
     setCurrentPage(page) {
@@ -17,7 +18,10 @@ export const usePostStore = defineStore({
     },
     setCurrentSelect(inputSelected) {
       this.currentSelect = inputSelected;
-
+    },
+    setCurrentSearch(value) {
+      alert('value')
+      this.currentSearch = value;
     },
   },
   getters: {
@@ -33,7 +37,29 @@ export const usePostStore = defineStore({
       }
     
       return filteredPosts;
-    }
-  }
-},
-);
+    },
+  
+    // filteredBySearch: (state) => {
+    //   let filteredPosts = [];
+      
+    //   for (const letter of state.currentSearch.toLowerCase()) {
+    //     filteredPosts = filteredPosts.filter(post => {
+    //       return post.title.includes(letter);
+    //     });
+    //   }
+      
+    //   return filteredPosts;
+    // },
+
+     
+    filteredBySearch: (state) => {
+      let filteredPosts = [];
+      console.log(state.currentSearch)
+        filteredPosts= state.postsData.filter(post => {
+          return post.title === state.currentSelect;
+        });
+     return filteredPosts;
+    } 
+   
+  
+  }});
