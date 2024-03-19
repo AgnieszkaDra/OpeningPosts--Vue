6eaf34328155eb3f6d29d9498e5202535a327b4c
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-03-19 09:36:22 +0100

    refactoring onMounted component method

--- src/App.vue
+++ src/App.vue
@@ -6,56 +6,38 @@
 <script setup>
 
 import NavigationComponent from './components/NavigationComponent'
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, watch } from 'vue';
 import { usePostStore } from '@/store';
 import postData from '../data/post';
 
 const postStore = usePostStore();
 
-onMounted(async () => {
-      try {
-        const postsArray = []
-        const isLoading = ref(true);
+onMounted(() => {
+  const postsArray = [];
 
-        for (const post of postData) {
-          const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              title: post.title,
-              body: post.body,
-              bodySecond: post.bodySecond,
-              background: post.background,
-              select: post.select,
-              author: post.author,
-              categories: post.categories,
-              userId: post.userId
-            }),
-          });
-
-          const data = await response.json();
-          postsArray.push(data);
-          isLoading.value = false;
-        }
-
-        postStore.$patch({
-          postsData: postsArray,
-          totalPages: Math.ceil(postsArray.length / postStore.postsPerPage)
-        });
-      } catch (error) {
-        console.error('Error fetching posts:', error);
-      }
+  for (const post of postData) {
+    postsArray.push({
+      title: post.title,
+      body: post.body,
+      bodySecond: post.bodySecond,
+      background: post.background,
+      select: post.select,
+      author: post.author,
+      categories: post.categories,
+      userId: post.userId
     });
+  }
+      
+  postStore.$patch({
+    postsData: postsArray,
+    totalPages: Math.ceil(postsArray.length / postStore.postsPerPage)
+  });
 
-    watch(() => postStore.currentSelect, () => {
-      postStore.filteredBySelect;
-    });
+  }) 
 
-    watch(postStore.currentSelect, () => {
+watch(postStore.currentSelect, () => {
     postStore.filteredBySelect
-    })
+})
 
 </script>
 
