49b0f178d9f9551a94d1f044fdc8d7e35b0fe015
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-07-08 08:16:21 +0200

    correct router

--- src/components/PostSimple.vue
+++ src/components/PostSimple.vue
@@ -8,23 +8,21 @@
     </div>
   </div>
 </template>
-
+ 
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import { usePostStore } from '@/store';
-import { useRoute } from 'vue-router'; 
 
 const postStore = usePostStore();
 const postsData = computed(() => postStore.postsData);
-const route = useRoute(); 
 const selectedPost = ref(null);
+const route = useRoute();
 
-const path = route.path;
-const segments = path.split('/');
-const lastSegment = segments[segments.length - 1];
-const decodedTitleValue = decodeURIComponent(lastSegment);
-
-selectedPost.value = postsData.value.find(post => post.title === decodedTitleValue);
+onMounted(() => {
+  const postTitle = route.params.postTitle;
+  selectedPost.value = postsData.value.find(post => post.title === postTitle);
+});
 
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -4,24 +4,10 @@ import PostSimple from '../components/PostSimple'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView  },
-  { path: '/Początek', name: 'Początek', component: PostSimple },
-  { path: '/Zając', name: 'Zając', component: PostSimple },
-  { path: '/Piłka', name: 'Piłka', component: PostSimple },
-  { path: '/Codzienność', name: 'Codzienność', component: PostSimple },
-  { path: '/Elegancja', name: 'Elegancja', component: PostSimple },
-  { path: '/Zając ukryty', name: 'Zając ukryty', component: PostSimple },
-  { path: '/Czapki', name: 'Czapki', component: PostSimple },
-  { path: '/Slow fashion', name: 'Slow fashion', component: PostSimple },
-  { path: '/Opaski', name: 'Opaski', component: PostSimple },
-  { path: '/Zima', name: 'Zima', component: PostSimple },
-  { path: '/Na Walentynki', name: 'Na Walentynki', component: PostSimple },
-  { path: '/Czereśnie', name: 'Czereśnie', component: PostSimple },
-  { path: '/Wieloryby', name: 'Wieloryby', component: PostSimple },
-  { path: '/Sukienka', name: 'Sukienka', component: PostSimple },
-  { path: '/Co słychać', name: 'Co słychać', component: PostSimple },
-  { path: '/Fiolet', name: 'Fiolet', component: PostSimple },
-  { path: '/Wilk', name: 'Wilk', component: PostSimple },
-  { path: '/Leśne zwierzęta', name: 'Leśne zwierzęta', component: PostSimple },
+  {
+    path: '/:postTitle',
+    component: PostSimple
+  },
 ];
 
 const router = createRouter({
